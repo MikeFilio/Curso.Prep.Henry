96c1acb81fb93ce144d9c9810901c5b2bf6c88b1
MikeFilio <creepf5@hotmail.com>
2022-07-04 19:14:23 -0500

    complete 02

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -81,13 +81,13 @@ function tienenMismaLongitud(str1, str2) {
   // Devuelve "true" si las dos strings tienen la misma longitud
   // De lo contrario, devuelve "false"
   // Tu código:
-  if (str1.lenght === str2.lenght){
+  if (str1.length === str2.length){
   return true;
+  }else {
+  return false;
   }
-    return false;
   
-  
-  
+   
 }
 
 function menosQueNoventa(num) {
@@ -214,20 +214,21 @@ function combinarNombres(nombre, apellido) {
   // Devuelve "nombre" y "apellido" combinados en una string y separados por un espacio.
   // Ejemplo: "Soy", "Henry" -> "Soy Henry"
   // Tu código:
-  return (nombre + ''+ apellido);
+  return (nombre + ` ` + apellido);
 }
 
 function obtenerSaludo(nombre) {
   // Toma la string "nombre" y concatena otras string en la cadena para que tome la siguiente forma:
   // "Martin" -> "Hola Martin!"
   // Tu código:
-  return ( 'hola'+nombre+'!');
+  return  (`Hola ` + nombre + `!`);
+
 }
 
 function obtenerAreaRectangulo(alto, ancho) {
   // Retornar el area de un rectángulo teniendo su altura y ancho
   // Tu código:
-  var resultado (alto * ancho);
+  var resultado =(alto * ancho);
   return resultado;
 
   
@@ -262,13 +263,14 @@ function esVocal(letra){
   //que no se puede procesar el dato mediante el mensaje "Dato incorrecto".
   // Si no es vocal, tambien debe devolver "Dato incorrecto".
   //Escribe tu código aquí
- if (letra.length>1){
-  return 'dato incorrecto'}
-  if (letra ==='a' || letra ==='e' || letra === ' i' || letra === ' 0' || letra === 'u' ){
+    if (letra ==='a' || letra ==='e' || letra === 'i' || letra === 'o' || letra === 'u' ){
     return 'Es vocal';
-  }
-  return 'dato incorrecto'
-  
+    } else if(letra.length>1){
+    return 'Dato incorrecto';
+    } else {
+      return 'Dato incorrecto'}
+
+ 
 }
 
 
